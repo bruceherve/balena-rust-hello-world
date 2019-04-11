303155cfd6e2f709cb1b19e56e21049575aed0e6
Robert Vojta <robert@balena.io>
2019-04-11 20:46:04 +0200

    Remove rust uname
    Signed-off-by: Robert Vojta <robert@balena.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg};
 use reqwest;
-use uname;
 
 fn main() {
     let matches = App::new(crate_name!())
@@ -9,9 +8,6 @@ fn main() {
         .about(crate_description!())
         .arg(Arg::with_name("name").index(1).required(true))
         .get_matches();
-
-    let info = uname::uname().unwrap();
-    println!("{:?}", info);
 
     let response = reqwest::get(matches.value_of("name").unwrap()).and_then(|mut x| x.text());
 
